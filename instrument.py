3b780ea68f12ff4e47dd70863b7ec7750425dcd4
Martin Duffy <martin.duffy@kitware.com>
2025-04-15 10:22:33 -0400

    Fix compiler errors on some platforms and add comments to callback

--- instrument.py
+++ instrument.py
@@ -5,21 +5,26 @@ import shutil
 import sys
 
 if __name__ == "__main__":
+    # The instrumentation feature will pass the path to an index file to our callback
     index = sys.argv[1]
 
+    # Get the buildDir and dataDir from the index file
     with open(index) as f:
         data = json.load(f)
         buildDir = data["buildDir"]
         dataDir = data["dataDir"]
 
+    # Get a unique output directory name based on the index filename
     indexName = os.path.basename(index).split(".")[0]
 
+    # Create an output directory that CMake won't clear, to copy our files into
     outputDir = os.path.join(buildDir, "instrumentation")
     indexDir = os.path.join(outputDir, indexName)
     for d in [outputDir, indexDir]:
         if not os.path.exists(d):
             os.makedirs(d)
 
+    # Copy all the instrumentation data into our output directory
     for snippet in data["snippets"]:
         shutil.copyfile(
             os.path.join(dataDir, snippet),
@@ -30,4 +35,5 @@ if __name__ == "__main__":
         os.path.join(indexDir, os.path.basename(index))
     )
 
+    # Generate our trace.json file
     main(index, os.path.join(indexDir, "trace.json"))
